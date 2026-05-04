dfc66b629e6eab950896a9d10f7c1465d621b4a1
Martzz-a <maritzafidelyah@gmail.com>
2026-05-04 15:40:18 +0700

    Menambahkan Implementasi Function di Luar Class

--- staticFuntion.cpp
+++ staticFuntion.cpp
@@ -27,3 +27,12 @@ public:
 };
 
 int Mahasiswa::nim = 0;
+
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+}
