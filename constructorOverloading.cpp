5f27e920436b1ca8b0d0b909015c1b0607e6c793
Martzz-a <maritzafidelyah@gmail.com>
2026-05-04 16:04:39 +0700

    Menambahkan Program Utama

--- constructorOverloading.cpp
+++ constructorOverloading.cpp
@@ -39,3 +39,15 @@ void Mahasiswa::cetak(){
     cout << endl << "Nim = " << nim << endl;
     cout << "Nama = " << nama << endl;
 }
+
+int main() {
+    Mahasiswa mhs1(102030);
+    Mahasiswa mhs2("Andi");
+    Mahasiswa mhs3(010203, "Santi");
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
